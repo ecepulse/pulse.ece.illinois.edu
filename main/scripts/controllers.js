978957ca63538b3cd7903fc9185df7622a873227
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-09-16 12:04:44 -0500

    Changed the structure a little

--- main/scripts/controllers.js
+++ main/scripts/controllers.js
@@ -1,5 +1,28 @@
 var ecePulse2016 = angular.module("ECEPulse2016", []);
 
-ecePulse2016.controller('splashPage', function($scope) {
+ecePulse2016.controller('splashPage', function ($scope) {
+    "use strict"; // To detect any coding errors
     
+    $scope.infoCards = [
+        {
+            "title": "One week of innovation.",
+            "info": "ECE Pulse is an annual student-run conference held at UIUC which showcases innovation in electrical and computer engineering technologies. The conference is comprised of design competitions, tech talks, and networking sessions for students, alumni, faculty, and corporate representatives."
+    }, {
+            "title": "Infinite opportunities.",
+            "info": "Our aim is to inspire students by connecting them with the latest in novel technologies and projects in the corporate world, exposing them to new opportunities for ECE within industry. Engineers from leading tech companies will engage attendees in discussions regarding the latest and greatest in ECE innovation.",
+    }, {
+            "title": "Engage. Network. Innovate.",
+            "info": "We encourage attendees to challenge themselves. Step out of your comfort zone. Meet new people. Learn from the best. Build teams. Collaborate."
+    }];
+    
+    $scope.sponsors = [
+        {
+            "resting": {
+                "imgLink": "assets/sponsors/TI.jpg",
+            },
+            "onHover": {
+                "imgLink": "assets/sponsor/TIhover.jpg",
+                "title": "Texas Instruments",
+            }
+        }];
 });
