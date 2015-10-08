5ff0085288596d37bb239cdf76e65be49510a6a7
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-10-07 21:38:59 -0500

    Fix

--- preregister.php
+++ preregister.php
@@ -1,25 +1,19 @@
 <?php
-$server_name = "pulseece.web.engr.illinois.edu";
+
+$servername = "pulseece.web.engr.illinois.edu";
 $username = "pulseece_admin16";
 $password = "ECEPulse123";
 $database = "pulseece_2016";
-
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error)
 {
-    echo "Failure";
     die("Connection failed: " . $conn->connect_error);
-}
-
-function saveEmail($email)
-{
-    $query = "INSERT INTO preregister (col1) VALUES(".$email.")";
-    $conn->query($query);
-    return "shouldBeSuccessful";
 }
 
 $email = $_POST["email"];
 
-echo saveEmail($email);
+$query = 'INSERT INTO preregister (email) VALUES("'.$email.'")';
+$conn->query($query);
+
 ?>
