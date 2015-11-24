1ae7819d2f30aa8173fd34026fce4179bab80a32
Anit Gandhi <anitgandhi@gmail.com>
2015-11-23 23:25:34 -0600

    space between splash and content

--- assets/pulse.js
+++ assets/pulse.js
@@ -1,56 +1,55 @@
 var lock = 0;
 
 function hackertyper() {
-        
-        if (lock == 1)
-            return;
-       
-        lock = 1;
-        var endings = ["_life", 
-                         "_kitchen", 
-                         "_roads", 
-                         "_school", 
-                         "_business", 
-                         "_world", 
-                         "_work"];
-        
-        var hacker = $('#hackertyper');
-        var newRand = hacker.html();
-        
-        while (newRand === hacker.html()) newRand = endings[Math.floor(Math.random() * endings.length)];
-        
-        var cache = hacker.html().length;
-        var i = 0;
-        var addCallbackId, removeCallbackId;
-        
-        var removeCallback = function() {
-            
-            if (i >= cache) {
-                clearInterval(removeCallbackId);
-                i = 0;
-                addCallbackId = setInterval(addCallback, 200);
-            }
-            for (; i < cache; i++) {
-                hacker.html(hacker.html().substr(0, hacker.html().length - 1));
-                i++;
-                return;
-            }
-        }
+    if (lock == 1)
+        return;
+   
+    lock = 1;
+    var endings = ["_life", 
+                     "_kitchen", 
+                     "_roads", 
+                     "_school", 
+                     "_business", 
+                     "_world", 
+                     "_work"];
     
-        removeCallbackId = setInterval(removeCallback, 200);
+    var hacker = $('#hackertyper');
+    var newRand = hacker.html();
+    
+    while (newRand === hacker.html()) newRand = endings[Math.floor(Math.random() * endings.length)];
+    
+    var cache = hacker.html().length;
+    var i = 0;
+    var addCallbackId, removeCallbackId;
+    
+    var removeCallback = function() {
         
-        var addCallback = function() {
-            if (i >= newRand.length) {
-                clearInterval(addCallbackId);
-                lock = 0;
-            }
-            for (; i < newRand.length; i++) {
-                hacker.html(hacker.html() + newRand[i]);
-                i++;
-                return;
-            }
+        if (i >= cache) {
+            clearInterval(removeCallbackId);
+            i = 0;
+            addCallbackId = setInterval(addCallback, 200);
+        }
+        for (; i < cache; i++) {
+            hacker.html(hacker.html().substr(0, hacker.html().length - 1));
+            i++;
+            return;
         }
     }
+
+    removeCallbackId = setInterval(removeCallback, 200);
+    
+    var addCallback = function() {
+        if (i >= newRand.length) {
+            clearInterval(addCallbackId);
+            lock = 0;
+        }
+        for (; i < newRand.length; i++) {
+            hacker.html(hacker.html() + newRand[i]);
+            i++;
+            return;
+        }
+    }
+}
 
 $(document).ready(function () {
     var interval = setInterval(hackertyper, 5000);
